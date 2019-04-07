92ab4e56c4ceb863141b63804e30d47b45dd555d
Arunscape <8227297+Arunscape@users.noreply.github.com>
2019-04-07 10:12:43 -0600

    currently panics on wayland

---
+++ README.md
@@ -0,0 +1,8 @@
+# wip
+
+currently, it panics on wayland.
+relevant issues: 
+
+https://github.com/tomaka/winit/issues/793
+
+https://github.com/ggez/ggez/issues/579
---
+++ src/main.rs
@@ -0,0 +1,35 @@
+// use ggez::*;
+
+// struct State {}
+
+// impl ggez::event::EventHandler for State {
+//   fn update(&mut self, ctx: &mut Context) -> GameResult<()> {
+//       Ok(())
+//   }
+//   fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
+//       Ok(())
+//   }
+// }
+
+
+// fn main() {
+//     let state = &mut State { };
+
+//     let c = conf::Conf::new();
+//     let (ref mut ctx, ref mut event_loop) = ContextBuilder::new("hello_ggez", "awesome_person")
+//     .conf(c)
+//     .build()
+//     .unwrap();
+
+//     event::run(ctx, event_loop, state).unwrap();
+// }
+
+
+extern crate winit;
+
+fn main() {
+    let mut events_loop = winit::EventsLoop::new();
+    let window = winit::Window::new(&events_loop).unwrap();
+
+    let monitor = winit::Window::get_current_monitor(&window);
+}
